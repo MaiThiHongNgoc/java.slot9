8c25ffbf1dbf70c0403ec0a7d34acf272b09327b
MaiThiNgoc <ngoc72887@gmail.com>
2024-05-18 14:31:32 +0700

    hn

---
+++ baithi2/DbConnection.java
@@ -0,0 +1,20 @@
+package baithi2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        String dbURL = "jdbc:mysql://localhost:3306/AddressBookDB";
+        conn = DriverManager.getConnection(dbURL,"root","");
+        return conn;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        if(getConnection()!= null){
+            System.out.printf("Connected");
+        }
+    }
+}
